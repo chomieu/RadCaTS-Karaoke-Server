0286e990801e923616b622de29c07a90b945e7a8
chomieu <chomieu@gmail.com>
2021-03-02 21:11:35 -0800

    Functional socket.io server rooms

--- index.js
+++ index.js
@@ -3,18 +3,39 @@ const app = express()
 const server = require("http").Server(app)
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://radcats-karaoke-app.herokuapp.com/liveSession",
+    // origin: "http://radcats-karaoke-app.herokuapp.com/liveSession",
     // origin: "http://radcats-karaoke-ui.herokuapp.com/api/session/:id",
-    // origin: "http://localhost:3000",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"]
   }
 })
 
+let users = []
+
 // Events handler
 io.on("connection", socket => {
   console.log("connected")
-  socket.on("play", playMsg => {
-    io.emit("play", playMsg)
+
+  socket.on("joinSession", (sessionId, userId, cb) => {
+    const user = {
+      sessionId: sessionId,
+      userId: userId,
+      socketId: socket.id
+    }
+    console.log("join", sessionId)
+    socket.join(sessionId)
+    users.push(user)
+    cb(users)
+  })
+
+  socket.on("start", (sessionId, playMsg) => {
+    console.log("start", sessionId)
+    io.to(sessionId).emit("play", playMsg)
+  })
+
+  socket.on("disconnect", () => {
+    users = users.filter(user => user.id !== socket.id)
+    io.emit("newMembers", users)
   })
 })
 
