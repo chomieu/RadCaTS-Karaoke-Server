acb37b048ba5fe48b95a9a203bdf2e77a0948721
Tim Keller <timothy.m.keller@gmail.com>
2021-03-03 19:28:02 -0800

    Removed unnecessary back button function for Session.js, renamed the improperly named Pause button to be right. Removed setSessionData from AudioPlayer handlePlay, and removed ternary operator to control pause button in AudioBottom.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const server = require("http").Server(app)
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://radcats-karaoke-app.herokuapp.com",
+    // origin: "http://radcats-karaoke-app.herokuapp.com",
     // origin: "http://radcats-karaoke-ui.herokuapp.com/api/session/:id",
-    // origin: "http://localhost:3000",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"]
   }
 })
